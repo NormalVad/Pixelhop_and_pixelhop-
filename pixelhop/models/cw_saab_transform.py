74e7f0e9cc3d8a92336440e425ab8851809b2d31
Your Name <ayushgoyal5720@gmail.com>
2025-04-29 01:53:40 -0700

    cw_saab

--- pixelhop/models/cw_saab_transform.py
+++ pixelhop/models/cw_saab_transform.py
@@ -13,53 +13,37 @@ class CWSaabTransform:
         self.energy_ratios = None
         
     def fit(self, X):
-        # Center the data
+        # Center data
         self.mean = np.mean(X, axis=0)
         X_centered = X - self.mean
-        
-        # Compute covariance matrix
+        # covariance matrix
         cov = np.cov(X_centered, rowvar=False)
-        
-        # Compute eigenvectors and eigenvalues
         eigenvalues, eigenvectors = np.linalg.eigh(cov)
-        
-        # Sort in descending order
         idx = np.argsort(eigenvalues)[::-1]
         eigenvalues = eigenvalues[idx]
         eigenvectors = eigenvectors[:, idx]
-        
-        # Calculate energy
+        # energy
         total_energy = np.sum(eigenvalues)
         self.energy_ratios = eigenvalues / total_energy
-        
-        # Determine intermediate and discarded nodes based on energy thresholds
+        # intermediate and discarded nodes based on energy thresholds
         intermediate_indices = np.where(self.energy_ratios >= self.energy_threshold_TH1)[0]
         discarded_indices = np.where((self.energy_ratios < self.energy_threshold_TH1) & 
                                    (self.energy_ratios >= self.energy_threshold_TH2))[0]
-        
         self.intermediate_nodes = len(intermediate_indices)
         self.discarded_nodes = len(discarded_indices)
-        
-        # Select kernels based on thresholds or specified number
+        # kernels
         if self.num_kernels is not None:
             selected_indices = np.arange(min(self.num_kernels, len(eigenvalues)))
         else:
             selected_indices = np.concatenate([intermediate_indices, discarded_indices])
-            
         self.kernels = eigenvectors[:, selected_indices]
-        
-        # Compute bias (DC component)
+        # bias
         self.bias = np.dot(self.mean, self.kernels)
-        
         return self
     
     def transform(self, X):
-        # Center the data
         X_centered = X - self.mean
-        
-        # Transform
         X_transformed = np.dot(X_centered, self.kernels)
-        
         return X_transformed
     
     def get_num_parameters(self):
